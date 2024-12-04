cbdeb9ade70a895a244b9355fcd1284ae326d489
ryan.bardahi <ryan.bardahi@gmail.com>
2024-12-04 20:14:15 +0800

    Add _id in get all post

--- controllers/post.js
+++ controllers/post.js
@@ -2,7 +2,7 @@ const Post = require('../models/Post');
 
 module.exports.getAllPosts = async (req, res) => {
     try {
-        const posts = await Post.find().populate('author', 'username');
+        const posts = await Post.find().populate('author', '_id username');
         res.status(200).json(posts);
     } catch (error) {
         res.status(500).json({ error: error.message });
